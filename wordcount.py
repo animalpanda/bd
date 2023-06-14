d6483fe3c4ea196340eb541fed4ec251d414e2fc
animalpanda <136642203+animalpanda@users.noreply.github.com>
2023-06-15 01:06:33 +0530

    Create wordcount.py

---
+++ wordcount.py
@@ -0,0 +1,42 @@
+# mapper
+
+import sys
+
+for line in sys.stdin:
+	line = line.strip()
+	words = line.split()
+
+	for word in words:
+		print("%s\t%s" % (word,1))
+    
+# reducer
+
+import sys
+
+current_word = None
+current_count = 0
+word = None
+
+
+for line in sys.stdin:
+
+	line = line.strip()
+	word, count = line.split('\t', 1)
+
+	try:
+		count = int(count)
+	except:
+		continue
+
+	if current_word == word:
+		current_count += count
+
+	else:
+		if current_word:
+			print("%s\t%s" % (current_word, current_count))
+
+		current_count = count
+		current_word = word
+
+if current_word == word:
+	print("%s \t %s" % (current_word, current_count))
